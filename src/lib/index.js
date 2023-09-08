0c0376531f8e5c2c124a40945593f8c7138e5b4f
Race Osuna <119973475+RaceOsuna@users.noreply.github.com>
2023-09-08 14:40:37 -0600

    Feat: firebase db

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,1 +1,9 @@
 // place files you want to import through the `$lib` alias in this folder.
+export const firebaseConfig = {
+  apiKey: "AIzaSyDJOi2grEK0fiwQ1uT9jWTRA5ff9oPnWzM",
+  authDomain: "todo-svelte-9f707.firebaseapp.com",
+  projectId: "todo-svelte-9f707",
+  storageBucket: "todo-svelte-9f707.appspot.com",
+  messagingSenderId: "994754449933",
+  appId: "1:994754449933:web:c5bbfc88ce1e7323f27b44"
+};
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,134 +1,157 @@
 <script>
-  let taskList = [
-    {name: 'test1', isComplete: false}, 
-    {name: 'test2', isComplete: false},
-    {name: 'test3', isComplete: false}
-]
+// @ts-nocheck
+
+import {
+  initializeApp
+} from "firebase/app";
+import {
+  getFirestore,
+  collection,
+  onSnapshot,
+  doc,
+  setDoc, 
+  deleteDoc,
+  addDoc
+} from "firebase/firestore";
+import {
+  firebaseConfig
+} from '$lib/index'
+
+const app = initializeApp(firebaseConfig)
+const db = getFirestore(app)
+
+const collect = collection(db, "taskList")
+
+let taskList = []
+
+const unsubscribe = onSnapshot(collect, (snapshot) => {
+  let fbtl = []
+  snapshot.forEach(item => {
+    let task = {
+      ...item.data(),
+      id: item.id
+    }
+    fbtl = [task, ...fbtl]
+  })
+  taskList = fbtl
+})
+
+console.log(taskList)
 
 let inputText = ''
 let updateText = ''
-// @ts-ignore
-	/**
-	 * @type {string | number | boolean | null}
-	 */
-let findIndex = null
-
-const addTask = () => {
-  let newTask = {
+let taskToUpdate = null
+ 
+const addTask = async () => {
+  const docRef = await addDoc(collection(db, "taskList"), {
   name: inputText,
   isComplete: false
-};
-  taskList = [...taskList, newTask];
-  inputText = ''
+});
 }
 
-// @ts-ignore
-const markComplete = (index) => {
-  if(!taskList[index].isComplete) {
-  taskList[index].isComplete = true
-  } else {
-    taskList[index].isComplete = false
-  }
+const markComplete = async (task) => {
+  await setDoc(doc(db, "taskList", task.id), {
+    ...task,
+    isComplete: !task.isComplete
+  });
 }
 
-// @ts-ignore
-const deleteTask = (index) => {
-  const deleteTask = taskList[index]
-  taskList = taskList.filter(task => task !== deleteTask)
+const deleteTask = async (task) => {
+  await deleteDoc(doc(db, "taskList", task.id))
 }
 
-// @ts-ignore
-const editTask = (index) => {
-  updateText = taskList[index].name
-  findIndex = index
-  console.log(findIndex)
+const editTask = (task) => {
+  updateText = task.name
+  taskToUpdate = task
 }
 
-const updateTask = () => {
-  // @ts-ignore
-  taskList[findIndex].name = updateText
-  updateText = ''
-  findIndex = false
+const updateTask = async() => {
+  await setDoc(doc(db, "taskList", taskToUpdate.id), {
+    ...taskToUpdate,
+    name: updateText
+  });
 }
-
 </script>
 
 <main>
-  <div class="app-container">
-    <h1>Todo 📒</h1>
-    <form>
-      <input type="text" bind:value={inputText}>
-      <button disabled={inputText ? false : true} on:click={addTask}>Add task</button>
-      <input type="text" disabled={updateText ? false : true} bind:value={updateText}>
-      <button disabled={findIndex ? false : true} on:click={updateTask}>Update</button>
-    </form>
-    {#if taskList.length}
-    <ol>
-      {#each taskList as task, index}
-      <div class="list-item">
-        <li class:complete={task.isComplete}>{task.name}</li>
-        <div class="icon-container">
-          <!-- svelte-ignore a11y-click-events-have-key-events -->
-          <span role="button" tabindex="0" class="icon" on:click={() => editTask(index)}>✏️</span>
-          <!-- svelte-ignore a11y-click-events-have-key-events -->
-          <span role="button" tabindex="0" class="icon" on:click={() => markComplete(index)}>✅</span>
-          <!-- svelte-ignore a11y-click-events-have-key-events -->
-          <span role="button" tabindex="0" class="icon" on:click={() => deleteTask(index)}>🗑️</span>
-        </div>
-      </div>
-      {/each}
-    </ol>
-    {:else}
-      <h2>No Tasks</h2>
-    {/if}
-  </div>
+    <div class="app-container">
+        <h1>Todo 📒</h1>
+        <form>
+            <input type="text" bind:value={inputText}>
+            <button disabled={inputText ? false : true} on:click={addTask}>Add task</button>
+            <input type="text" disabled={updateText ? false : true} bind:value={updateText}>
+            <button disabled={taskToUpdate ? false : true} on:click={updateTask}>Update</button>
+        </form>
+        {#if taskList.length}
+        <ol>
+            {#each taskList as task, index}
+            <div class="list-item">
+                <li class:complete={task.isComplete}>{task.name}</li>
+                <div class="icon-container">
+                    <!-- svelte-ignore a11y-click-events-have-key-events -->
+                    <span role="button" tabindex="0" class="icon" on:click={() => editTask(task)}>✏️</span>
+                    <!-- svelte-ignore a11y-click-events-have-key-events -->
+                    <span role="button" tabindex="0" class="icon" on:click={() => markComplete(task)}>✅</span>
+                    <!-- svelte-ignore a11y-click-events-have-key-events -->
+                    <span role="button" tabindex="0" class="icon" on:click={() => deleteTask(task)}>🗑️</span>
+                </div>
+            </div>
+            {/each}
+        </ol>
+        {:else}
+        <h2>No Tasks</h2>
+        {/if}
+    </div>
 </main>
 
 <style>
-  main {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    width: 100%;
-    margin: 0;
-  }
+main {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  margin: 0;
+}
 
-  .app-container {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    width: 50%;
-  }
-  .complete {
-    text-decoration: line-through;
-    color: red;
-  }
+.app-container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 50%;
+}
 
-  ol {
-    width: 100%;
-    padding-left: 0;
-  }
+.complete {
+  text-decoration: line-through;
+  color: red;
+}
 
-  .list-item {
-    display: flex;
-  }
-  li {
-    display: flex;
-    justify-content: space-between;
-    width: 100%;
-  }
+ol {
+  width: 100%;
+  padding-left: 0;
+}
 
-  .icon-container {
-    display: flex;
-    justify-content: space-around;
-    width: 30%;
-  }
+.list-item {
+  display: flex;
+}
 
-  button {
-    cursor: pointer;
-  }
+li {
+  display: flex;
+  justify-content: space-between;
+  width: 100%;
+  font-size: x-large;
+}
 
-  .icon {
-    cursor: pointer;
-  }
+.icon-container {
+  display: flex;
+  justify-content: space-around;
+  width: 30%;
+}
+
+button {
+  cursor: pointer;
+}
+
+.icon {
+  cursor: pointer;
+}
 </style>
