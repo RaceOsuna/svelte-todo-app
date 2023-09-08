1999740e991dfebef6e881d17a74e4b4762f20c0
Race Osuna <119973475+RaceOsuna@users.noreply.github.com>
2023-09-08 11:09:43 -0600

    Feat: functional task list

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,49 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+  let taskList = [
+    {name: 'test1', isComplete: false}, 
+    {name: 'test2', isComplete: true},
+    {name: 'test3', isComplete: false}
+]
+
+let inputText = ''
+
+const addTask = () => {
+  let newTask = {
+  name: inputText,
+  isComplete: false
+};
+  taskList = [...taskList, newTask];
+  inputText = ''
+}
+
+</script>
+
+<main>
+  <div>
+    <h1>Todo 📒</h1>
+    <form action="">
+      <input type="text" bind:value={inputText}>
+      <button on:click={addTask}>Add task</button>
+    </form>
+    <ol>
+      {#each taskList as task}
+      <li class:complete={task.isComplete}>{task.name}</li>
+      {/each}
+    </ol>
+  </div>
+</main>
+
+<style>
+  main {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+  .complete {
+    text-decoration: line-through;
+  }
+
+  button {
+    cursor: pointer;
+  }
+</style>
